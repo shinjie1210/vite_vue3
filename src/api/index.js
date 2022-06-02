0156690c6616bac433377bc05ecc546b823a9345
沈兴宇 <shinjie1210@foxmail.com>
2022-06-02 18:21:52 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,5 @@ import ApiManager from '../api/ApiManager'
 const apiManager = ApiManager.getApiHost("", "");
 //admin-auth-center/login         用户登录
 export function userLogin(data) {
-    return apiManager.post('https://jfvideo4-dev.zgpajf.com.cn' + "/admin-auth-center/system/login", data)
+    return apiManager.post('https://baidu.com.cn' + "/test", data)
 }
